8e8a894661ee51d018ccb8bfa689243ae684aed5
Precious <Preshomondi@gmail.com>
2022-07-16 02:20:12 +0300

    commit tree a302a2919e16eba1d0777d818677cf5746823670

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
 // Code EyesOnMe Component Here
+import React from "react"
+
+function EyesOnMe(){
+    function focusEl() {
+        console.log('Good!')
+    }
+    function blurEl(){
+        console.log('Hey! Eyes on me!')
+    }
+    return (
+        <button onFocus = {focusEl} onBlur = {blurEl}>
+            Eyes on me
+        </button>
+    )
+}
+
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,18 @@
 // Code Keypad Component Here
+import React from "react"
+
+function Keypad () {
+    function handleChange(event) {
+        console.log(`${event.target.name}`)
+    }
+    return(
+        <input
+        name="Entering password..."
+        type="password"
+        placeholder ="Enter password"
+        onChange={handleChange}
+        />
+       
+    )
+}
+export default Keypad
